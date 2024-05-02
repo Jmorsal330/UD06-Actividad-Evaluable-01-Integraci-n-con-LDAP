d8a69dfdd50ff47bef4e5b42aa78455268bef8a5
Jmorsal330 <jmorsal330@g.educaand.es>
2024-05-02 17:57:56 +0200

    update

---
+++ daw-spring-authenticating-ldap/src/main/java/org/iesalixar/daw2/dawspringauthenticatingldap/HomeController.java
@@ -0,0 +1,10 @@
+package org.iesalixar.daw2.dawspringauthenticatingldap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+}
